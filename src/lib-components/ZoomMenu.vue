f5246cf2a39e52b9da7176ab903ab074af968956
Taavi <taavi@root>
2024-04-16 18:16:56 +0300

    Fix zoom out button

--- src/lib-components/ZoomMenu.vue
+++ src/lib-components/ZoomMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wf-component wf-zoommenu">
         <div class="wf-item wf-zoom-in" v-touch:start="touchStart(1)" v-touch:end="touchStop()" @mousedown="start(1)" @mouseleave="stop()" @mouseup="stop()">+</div>
-        <div class="wf-item wf-zoom-out" v-touch:start="touchStart(1)" v-touch:end="touchStop()" @mousedown="start(-1)" @mouseleave="stop()" @mouseup="stop()">-</div>
+        <div class="wf-item wf-zoom-out" v-touch:start="touchStart(-1)" v-touch:end="touchStop()" @mousedown="start(-1)" @mouseleave="stop()" @mouseup="stop()">-</div>
     </div>
 </template>
 <script>
