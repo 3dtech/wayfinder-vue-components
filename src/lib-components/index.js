b45e0accd8495148d04ea86b9aff622dd42f1595
Taavi <taavi.juursalu@3dwayfinder.com>
2025-01-15 11:10:59 +0200

    Rebuild

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -118,6 +118,7 @@ export { default as WFPOI } from './items/POI.vue';
 export { default as WFPOIAdvertisement } from './items/POIAdvertisement.vue';
 export { default as WFGroup } from './items/Group.vue';
 export { default as WFPage } from './items/Page.vue';
+export { default as WFPageContent } from './items/PageContent.vue';
 export { default as WFTranslate } from './items/Translate.vue';
 export { default as WFYAH } from './YAH.vue';
 export { default as WFIcon } from './items/Icon.vue';
