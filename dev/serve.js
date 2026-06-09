8f5da7420d3a0887f7e61f573991d1f8831ce48a
Taavi <taavi.juursalu@3dwayfinder.com>
2026-06-09 22:22:32 +0300

    Fix route chooser option list when mounted

--- dev/serve.js
+++ dev/serve.js
@@ -18,9 +18,9 @@ Vue.use(Vue2TouchEvents,
   })
 
 Vue.config.productionTip = false;
-const WF_MAP_TYPE = "2d";
+const WF_MAP_TYPE = "3d";
 const WF_API = "live";
-const WF_PROJECT = "074292ce1a9d46d5736492d77182c501";
+const WF_PROJECT = "8aac0a8fcdaf1f3e720b6b0d89775a00";
 const customAPI = "http://localhost:3333";
 
 /* global WF_MAP_TYPE WayfinderAPI wayfinder*/
--- src/lib-components/RouteChooser.vue
+++ src/lib-components/RouteChooser.vue
@@ -6,13 +6,13 @@
     <div>
         <WFDropDown @select="endSelected" :options="options" valueKey="key"/>
     </div>
-    <div>
+    <div v-if="showAccessabilityToggle">
         <div class="wf-checkbox wf-accessibility" @click="toggleAccessibility">
             <input type="checkbox" v-model="accessibilityRoutesOnly" @click.stop />
             <WFTranslate k="widget-route-chooser-accessibility">Show accessible routes only</WFTranslate>
         </div>
     </div>
-    <div>
+    <div v-if="showButton">
         <button @click="showPath" class="wf-button">
             <WFTranslate k="widget-route-chooser-show-path" :disabled="this.poiA == null || this.poiB == null">Show Route</WFTranslate>
         </button>
@@ -37,6 +37,14 @@ export default {
 		showTitle: {
             type: Boolean,
 			default: false
+        },
+        showButton: {
+            type: Boolean,
+            default: true
+        },
+        showAccessabilityToggle: {
+            type: Boolean,
+            default: true
         }
     },
     data () {
@@ -51,13 +59,16 @@ export default {
             }]
 		}
 	},
+    mounted () {
+        this.updateList();
+    },
     methods: {
 		startSelected (option) {
             console.log('startSelected', option)
             this.poiA = option.raw;
         },
 		endSelected (option) {
-            console.log('endSelected', option)
+            console.log('endSelected', option);
             this.poiB = option.raw;
         },
         showPath () {
@@ -67,12 +78,10 @@ export default {
         },
         toggleAccessibility () {
             this.accessibilityRoutesOnly = !this.accessibilityRoutesOnly;
-        }
-    },
-    watch: {
-		pois: function () {
-            console.log('pois', this.pois);
-			if (this.pois && Array.isArray(this.pois)) {
+        },
+
+        updateList () {
+            if (this.pois && Array.isArray(this.pois)) {
                 this.options = this.pois.map((p) => {
                     return {
                         key: p.id,
@@ -81,6 +90,11 @@ export default {
                     }
                 })
             }
+        }
+    },
+    watch: {
+		pois: function () {
+			this.updateList();
 		}
 	},
 };
